cb340dcfec8717cf8050836b8d69cff169554b36
Exequiel Arco <exequielarco23@gmail.com>
2023-10-10 21:01:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("test");
+const videoContainer = document.querySelector(".video-container");
+const switchBtn = document.querySelector(".switch-btn");
+const preloader = document.querySelector(".preloader");
+
+switchBtn.addEventListener("click", () => {
+  if (!switchBtn.classList.contains("slide")) {
+    switchBtn.classList.add("slide");
+    videoContainer.pause();
+  } else {
+    switchBtn.classList.remove("slide");
+    videoContainer.play();
+  }
+});
+
+window.addEventListener("load", () => {
+  preloader.classList.add("hide-preloader");
+});
